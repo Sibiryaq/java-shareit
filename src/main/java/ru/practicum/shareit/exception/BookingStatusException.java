4ac52f11c5da33c7f1696f32771f3f5b5a7f1c12
Sibiryaq <alexander.tolst1kov@yandex.ru>
2023-10-09 08:10:31 +0700

    Sp.16 - 1st review

--- src/main/java/ru/practicum/shareit/exception/BookingStatusException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class BookingStatusException extends RuntimeException {
-
-    public BookingStatusException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exception/EmailDuplicationException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class EmailDuplicationException extends RuntimeException {
-    public EmailDuplicationException(String message) {
-        super(message);
-    }
-}
